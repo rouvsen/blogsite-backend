4d16a39fcba6bc7c09453e5400208815662ca513
rovshan <rouvsen@gmail.com>
2023-11-19 02:55:28 +0400

    Streamline Question Entity on the project

--- src/main/java/com/rouvsen/blogwebsitebackend/domain/Question.java
+++ src/main/java/com/rouvsen/blogwebsitebackend/domain/Question.java
@@ -2,6 +2,7 @@ package com.rouvsen.blogwebsitebackend.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class Question {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String fullName;
